0ba0b1651a0ed46c89594326aa1761f0c929e1f9
nehaaero <124447362+nehaaero@users.noreply.github.com>
2023-02-19 23:11:30 +0530

    Add files via upload

---
+++ Count of Even and odd numbers-Assigment-1.py
@@ -0,0 +1,15 @@
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+count_odd = 0
+count_even = 0
+for x in numbers:
+ if x % 2==0:
+ 	 count_even+=1
+ else:
+     count_odd+=1   
+print("even count in numbers is :",count_even)
+print("odd count in numbers is :",count_odd)  
+
+
+
+
+
